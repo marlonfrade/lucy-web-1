66253faaeea5a911d8af7b2ee9ec301bb3eeac39
Marlon Frade <marlonmelo98@hotmail.com>
2023-07-30 00:34:13 -0300

    feat (dashboard) : create navbar component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,15 @@
+import Navbar from "@/components/navbar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="relative h-full">
       <div className="z-[80] hidden h-full bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col">
         <p className="text-white"> Hello Sidebar</p>
       </div>
-      <main className="md:pl-72">{children}</main>
+      <main className="md:pl-72">
+        <Navbar />
+        {children}
+      </main>
     </div>
   );
 };
