02722612b242828c7616b677c0ad3ea028cc64e7
Marlon Frade <marlonmelo98@hotmail.com>
2023-08-03 16:04:51 -0300

    feat(dashboard/code): create code page layout

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { ChatCompletionRequestMessage } from "openai";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+import { Heading } from "@/components/heading";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+import { UserAvatar } from "@/components/user-avatar";
+import { LucyAvatar } from "@/components/lucy-avatar";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+
+import { formSchema } from "./constants";
+
+import { Code } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+
+const CodePage = () => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      const userMessage: ChatCompletionRequestMessage = {
+        role: "user",
+        content: values.prompt,
+      };
+      const newMessages = [...messages, userMessage];
+
+      const response = await axios.post("/api/conversation", {
+        messages: newMessages,
+      });
+
+      setMessages((current) => [...current, userMessage, response.data]);
+      form.reset();
+    } catch (error) {
+      // TODO: open premium plan modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Lucy DEV"
+        description="Gere códigos ou tire suas dúvidas com Lucy DEV."
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid w-full grid-cols-12 gap-2 rounded-lg border p-4 px-3 focus-within:shadow-sm md:px-6"
+            >
+              {" "}
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Lucy, como criar uma API REST ?"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 w-full lg:col-span-2"
+                disabled={isLoading}
+              >
+                Enviar
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="mt-4 space-y-4">
+          {isLoading && (
+            <div className="mx-auto flex w-full items-center justify-center rounded-lg bg-violet-500/10 p-8">
+              <Loader />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+            <Empty label="Qual código você precisa ?" />
+          )}
+          <div className="flex flex-col-reverse gap-y-4">
+            {messages.map((message) => (
+              <div
+                key={message.content}
+                className={cn(
+                  "flex w-full items-start gap-x-8 rounded-lg p-8",
+                  message.role === "user"
+                    ? "border border-black/10 bg-white"
+                    : "bg-muted",
+                )}
+              >
+                {message.role === "user" ? <UserAvatar /> : <LucyAvatar />}
+                <p className="text-sm ">{message.content}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CodePage;
--- components/empty.tsx
+++ components/empty.tsx
@@ -1,15 +1,30 @@
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 interface EmptyProps {
   label: string;
 }
 
 export const Empty = ({ label }: EmptyProps) => {
+  const pathname = usePathname();
+
+  const getEmptyImage = () => {
+    switch (pathname) {
+      case "/conversation":
+        return (
+          <Image alt="empty conversation" fill src="/conversation-empty.png" />
+        );
+      case "/code":
+        return <Image alt="empty code" fill src="/code-empty.png" />;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="flex h-full flex-col items-center p-20">
-      <div className="relative h-72 w-72">
-        <Image alt="empty" fill src="/empty.png" />
-      </div>
+      <div className="relative h-72 w-72">{getEmptyImage()}</div>
       <p className="text-center text-sm text-muted-foreground">{label}</p>
     </div>
   );
