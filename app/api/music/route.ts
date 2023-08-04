dad8caf98582af0714c92ca2d6c15f597f11aefc
Marlon Frade <marlonmelo98@hotmail.com>
2023-08-04 14:39:46 -0300

    feat(api/music): add music route for replicate

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 import Replicate from "replicate";
 
 const replicate = new Replicate({
-  auth: process.env.REPLICATE_API_KEY!,
+  auth: process.env.REPLICATE_API_TOKEN!,
 });
 
 export async function POST(req: Request) {
