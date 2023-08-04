08e9f1321b700bcd3eee656671b8874bd8c8d451
Marlon Frade <marlonmelo98@hotmail.com>
2023-08-04 01:57:10 -0300

    feat(dashboard/music): create music page

--- app/(dashboard)/(routes)/music/constants.ts
+++ app/(dashboard)/(routes)/music/constants.ts
@@ -1,5 +1,7 @@
 import * as z from "zod";
 
 export const formSchema = z.object({
-  prompt: z.string().min(1, { message: "O prompt deve ser preenchido" }),
+  prompt: z
+    .string()
+    .min(1, { message: "O prompt da música deve ser preenchido" }),
 });
