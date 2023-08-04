9f5661d12236645114396c4102b907e14247813b
Marlon Frade <marlonmelo98@hotmail.com>
2023-08-04 02:24:44 -0300

    feat(api/music): add replicate integration

--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -1,39 +1,38 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { Configuration, OpenAIApi } from "openai";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_KEY!,
 });
-
-const openai = new OpenAIApi(configuration);
 
 export async function POST(req: Request) {
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { messages } = body;
+    const { prompt } = body;
 
     if (!userId) {
       return new NextResponse("Não autorizado", { status: 401 });
     }
 
-    if (!configuration.apiKey) {
-      return new NextResponse("Chave OPENAI não configurada", { status: 500 });
+    if (!prompt) {
+      return new NextResponse("O prompt deve ser preenchido", { status: 401 });
     }
 
-    if (!messages) {
-      return new NextResponse("Mensagem vazia", { status: 400 });
-    }
+    const response = await replicate.run(
+      "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      },
+    );
 
-    const response = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages,
-    });
-
-    return NextResponse.json(response.data.choices[0].message);
+    return NextResponse.json(response);
   } catch (error) {
-    console.log("[CONVERSATION_ERROR]", error);
+    console.log("[MUSIC_ERROR]", error);
     return new NextResponse("Erro interno", { status: 500 });
   }
 }
