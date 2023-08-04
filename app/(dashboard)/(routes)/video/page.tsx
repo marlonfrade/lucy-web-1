b22b6b2ada10ed2027711fb3d0a34e4c2586bf0e
Marlon Frade <marlonmelo98@hotmail.com>
2023-08-04 18:04:36 -0300

    feat(dashboard/video): create dashboard video page

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import axios from "axios";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+
+import { Heading } from "@/components/heading";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+
+import { formSchema } from "./constants";
+
+import { VideoIcon } from "lucide-react";
+
+const VideoPage = () => {
+  const router = useRouter();
+  const [video, setVideo] = useState<string>();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setVideo(undefined);
+
+      const response = await axios.post("/api/video", values);
+
+      setVideo(response.data[0]);
+
+      form.reset();
+    } catch (error) {
+      // TODO: open premium plan modal
+      console.log(error);
+    } finally {
+      router.refresh();
+    }
+  };
+
+  return (
+    <div>
+      <Heading
+        title="Lucy Studio"
+        description="crie seu vídeo usando a tecnologia da Lucy, insira um prompt e a Lucy vai te ajudar a criar o seu vídeo."
+        icon={VideoIcon}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="grid w-full grid-cols-12 gap-2 rounded-lg border p-4 px-3 focus-within:shadow-sm md:px-6"
+            >
+              {" "}
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Lucy, crie um vídeo de um macaco voando em um foguete no espaço"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 w-full lg:col-span-2"
+                disabled={isLoading}
+              >
+                Enviar
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="mt-4 space-y-4">
+          {isLoading && (
+            <div className="mx-auto flex w-full items-center justify-center rounded-lg bg-violet-500/10 p-8">
+              <Loader />
+            </div>
+          )}
+          {!video && !isLoading && (
+            <Empty label="Qual vídeo você quer criar ?" />
+          )}
+          {video && (
+            <video
+              className="mt-8 aspect-video w-full rounded-lg border bg-black"
+              controls
+            >
+              <source src={video} />
+            </video>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoPage;
