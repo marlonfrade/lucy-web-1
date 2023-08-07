7e4aa117fb20256509c822f256d40d476d0a1a99
Marlon Frade <marlonmelo98@hotmail.com>
2023-08-07 15:05:13 -0300

    style (dashboard/layout): change sidebar z-index

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
 
   return (
     <div className="relative h-full">
-      <div className="z-[80] hidden h-full bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col">
+      <div className="hidden h-full bg-gray-900 md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col">
         <Sidebar apiLimitCount={apiLimitCount} />
       </div>
       <main className="md:pl-72">
