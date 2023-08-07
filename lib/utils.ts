67fb13861a404e529742bf0fb2a9a4be957ea9e4
Marlon Frade <marlonmelo98@hotmail.com>
2023-08-07 17:43:39 -0300

    feat(api): create stripe checkout and billing payment

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,22 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Generates a class name by merging multiple class values.
+ *
+ * @param {...ClassValue[]} inputs - The class values to merge.
+ * @return {string} - The merged class name.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/**
+ * Generates an absolute URL by concatenating the base URL with the given path.
+ *
+ * @param {string} path - The path to be appended to the base URL.
+ * @return {string} The absolute URL formed by combining the base URL and the path.
+ */
+export function absoluteUrl(path: string) {
+  return `${process.env.NEXT_PUBLIC_BASE_URL}${path}`;
 }
